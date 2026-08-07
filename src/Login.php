68404c0f606fe3aaae070c6291808e8ca3d3c863
LankoDm <play585588@gmail.com>
2026-08-07 12:33:50 +0300

    add login logic

---
+++ src/Login.php
@@ -0,0 +1,29 @@
+<?php
+
+function loginUser($dbConnection)
+{
+    if (empty($_POST['email']) || empty($_POST['password'])) {
+        $_SESSION['ErrorMessage'] = ["Всі поля обов'язкові!"];
+        header("Location: /login");
+        exit;
+    }
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email = :email";
+
+    $user = $dbConnection->prepare($query);
+    $user->execute(['email' => $email]);
+    $dataUser = $user->fetch();
+
+    if($dataUser && password_verify($password, $dataUser['password_hash'])){
+        $_SESSION['user']['id'] = $dataUser['id'];
+        header('Location: /');
+        exit;
+    }else{
+        $_SESSION['ErrorMessage'] = ["Неправильний логін або пароль"];
+        header("Location: /login");
+        exit;
+    }
+}
